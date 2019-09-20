0dbc2364678ba1999164ca9cf794122ad6617aff
crawsome <c3g6b2@gmail.com>
2019-09-19 23:13:31 -0700

    Create wow_translator.py

---
+++ wow_translator.py
@@ -0,0 +1,41 @@
+ally_horde = {
+    'A': 'Í',
+    'B': '□',
+    'C': 'À',
+    'E': 'b',
+    'F': '',
+    'G': '℮',
+    'H': '╤',
+    'I': 'ф',
+    'K': 'À',
+    'L': '♫',
+    'M': 'ñ',
+    'N': 'Ћ',
+    'O': 'm',
+    'R': 'Ç',
+    'S': 'ƒ',
+    'T': 'Ё',
+    'U': 'p',
+    'V': 'ê',
+    'W': '№',
+    'Y': 'd',
+    ' ': ' ',
+    '\'': '\''
+}
+
+def allie_to_horde(pt):
+    ct = ''
+    for letter in str(pt):
+        if letter in ally_horde:
+            ct += ally_horde[letter]
+    return ct
+
+
+def __main__():
+    pt = str(input('Alliance: '))
+    ct = allie_to_horde(pt)
+    print('Horde: ' + str(ct))
+
+
+if __name__ == '__main__':
+    __main__()
